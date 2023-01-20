5e21ebd0e4d3d16f85a2e7f325bd7c6f5a1592cc
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-20 15:01:41 +0200

    Create TaskList.js

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,35 @@
+import { useSelector } from 'react-redux';
+import { Task } from 'components/Task/Task';
+// import { selectTasks, selectStatusFilter } from 'redux/selectors';
+// import { statusFilters } from 'redux/constants';
+import css from './TaskList.module.css';
+import { selectVisibleTasks } from 'redux/selectors';
+
+// const getVisibleTasks = (tasks, statusFilter) => {
+//   switch (statusFilter) {
+//     case statusFilters.active:
+//       return tasks.filter(task => !task.completed);
+//     case statusFilters.completed:
+//       return tasks.filter(task => task.completed);
+//     default:
+//       return tasks;
+//   }
+// };
+
+export const TaskList = () => {
+  // const tasks = useSelector(selectTasks);
+  // const statusFilter = useSelector(selectStatusFilter);
+  // const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
+  const tasks = useSelector(selectVisibleTasks);
+
+  return (
+    <ul className={css.list}>
+      {tasks.map(task => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
